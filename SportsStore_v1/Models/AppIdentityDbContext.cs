fcb9e881789a7ee0c46709c9789d07216f5a1938
mbbipul <mbbipulmandol.cse3.bu@gmail.com>
2019-11-22 01:44:42 +0600

    Add AppIdentity DbContetxt

---
+++ SportsStore_v1/Models/AppIdentityDbContext.cs
@@ -0,0 +1,11 @@
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SportsStore.Models {
+    public class AppIdentityDbContext : IdentityDbContext<IdentityUser> {
+        public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options)
+                :base(options) { }
+    }
+}
